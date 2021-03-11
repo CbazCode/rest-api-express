1e2529bf1698992df645c0ca4b2bd3020639703d
Sebastian Asis <sasisromero10@gmail.com>
2021-03-10 19:25:04 -0500

    :dart: IMPROVE: cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const { dbConnection } = require('./database/config.js');
+const cors = require('cors');
 const app = express();
 
 dbConnection();
@@ -11,12 +12,12 @@ dbConnection();
 app.set('PORT', 3000 || process.env.PORT);
 
 // Middlewares
+
 // app.use('/posts',(req, resp, next)=> {
 //     console.log('Running middleware in posts');
 //     next();
 // })
-
-
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
